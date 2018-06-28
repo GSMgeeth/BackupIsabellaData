3c3ef1f4876d8e1d5eb119738fd5c75774b16ec9
Geeth sandaru Madhushan <geeth.gsm@gmail.com>
2018-06-28 16:53:36 +0530

    Add button and text area

---
+++ BackupIsabellaData/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace BackupIsabellaData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addFileBtn = new System.Windows.Forms.Button();
+            this.DataInfoTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // addFileBtn
+            // 
+            this.addFileBtn.Location = new System.Drawing.Point(300, 37);
+            this.addFileBtn.Name = "addFileBtn";
+            this.addFileBtn.Size = new System.Drawing.Size(130, 32);
+            this.addFileBtn.TabIndex = 0;
+            this.addFileBtn.Text = "Add Data File";
+            this.addFileBtn.UseVisualStyleBackColor = true;
+            // 
+            // DataInfoTxtBox
+            // 
+            this.DataInfoTxtBox.Location = new System.Drawing.Point(13, 121);
+            this.DataInfoTxtBox.Multiline = true;
+            this.DataInfoTxtBox.Name = "DataInfoTxtBox";
+            this.DataInfoTxtBox.Size = new System.Drawing.Size(775, 317);
+            this.DataInfoTxtBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DataInfoTxtBox);
+            this.Controls.Add(this.addFileBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addFileBtn;
+        private System.Windows.Forms.TextBox DataInfoTxtBox;
+    }
+}
+
---
+++ BackupIsabellaData/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BackupIsabellaData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BackupIsabellaData")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c8d191b2-328d-41a8-8ba3-4b15bd0edbd6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
